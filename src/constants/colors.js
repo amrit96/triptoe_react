f6a6437d7b502b8ea5db839a391ad03e4a70ec96
Amritayan Banerjee <amritayanbanerjee@Amritayans-Mac.local>
2025-05-04 03:48:55 +0530

    constants

---
+++ src/constants/colors.js
@@ -0,0 +1,26 @@
+const COLORS = {
+    light : {
+        BACKGROUND: "#fefae0",
+        TEXT: "#000000",
+        TEXT_SECONDARY: "#f4f3ee",
+        SECONDARY: "#003135",
+        PRIMARY: "#afdde5",
+    },
+    dark: {
+        BACKGROUND: "#463f3a",
+        TEXT: "#f4f3ee",
+        TEXT_SECONDARY: "#000000",
+        SECONDARY: "#0fa4af",
+        PRIMARY: "#024950"
+    },
+    logo: {
+        ORANGE: "#f37e57",
+        TEAL: "#09464d",
+        MINT: "#4ed5d2",
+    },
+    common: {
+        TERTIARY: "#964734",
+    },
+}
+
+export default COLORS
